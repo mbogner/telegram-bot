321273f1d208d8c5b6b08e6ced66c8cd25007d43
Manuel Bogner <manuel.bogner@openresearch.com>
2022-03-15 02:20:03 +0100

    tests require @Autowired

--- bot/src/test/kotlin/dev/mbo/telegrambot/TelegramBotApplicationTests.kt
+++ bot/src/test/kotlin/dev/mbo/telegrambot/TelegramBotApplicationTests.kt
@@ -18,10 +18,11 @@ package dev.mbo.telegrambot
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class TelegramBotApplicationTests(
+class TelegramBotApplicationTests @Autowired constructor(
     private var app: TelegramBotApplication
 ) {
 
